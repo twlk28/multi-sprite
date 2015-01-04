f6485728f5e92c6fa148af4caf477f6cb24fa263
twlk28 <twlk28@gmail.com>
2015-01-04 16:02:24 +0800

    support 3x sprite

--- index.js
+++ index.js
@@ -1,158 +1,117 @@
 var spritesmith = require('spritesmith'),
     fs = require('fs'),
-    file = require('file'),
     path = require('path'),
     _ = require('lodash'),
+    glob = require('glob'),
     async = require('async'),
     gm = require('spritesmith/node_modules/gmsmith/node_modules/gm');
 
+// Export the SpriteMaker function
 module.exports = function (options) {
-
     "use strict";
 
-    // Export the SpriteMaker function
-
-    var spritelist = [],
-        imageReplaces = [],
-        retinaSpritelist = [],
-        retinaImageReplaces = [],
-        csslist ,
-        RETINA_SUFFIX = '@2x';
+    var configItems = [],
+        csslist = [],
+        imageReplaces = {},
+        RETINA_2X_SUFFIX = '@2x',
+        RETINA_3X_SUFFIX = '@3x';
 
     // Normalize data
-    var successCB = options.successCB || function(){};
+    var successCB = options.successCB || function(){},
+        spriteSrcDIR = options.srcImg,
+        spriteDestDIR = options.destImg,
+        cssSrcDIR = options.srcCss,
+        cssDestDIR = options.destCss;
+
+    // 1. Prepare spirteConfigData
     (function () {
-        var spriteSrcDIR = options.srcImg,
-            spriteDestDIR = options.destImg,
-            cssSrcDIR = options.srcCss,
-            cssDestDIR = options.destCss,
-            exclude = [RETINA_SUFFIX, '.DS_Store', '.svn'],
-            include = [RETINA_SUFFIX];
+        configItems = configItems.concat(dir2config('./'))
+        glob.sync('./*/', {
+            cwd: path.join(process.cwd(), spriteSrcDIR)
+        }).forEach(function (subdir) {
+            configItems = configItems.concat(dir2config(subdir))
+        })
+        configItems = _.filter(configItems, function(e){
+            return !_.isEmpty(e)
+        })
 
-        if(listDIR(spriteSrcDIR).length>0){
-            spritelist = listDIR(spriteSrcDIR).map(function (itemDIR) {
-                var src = listFileExclude(path.join(spriteSrcDIR, itemDIR), exclude).map(function(sliceFile){
-                    return path.join(spriteSrcDIR, itemDIR, sliceFile);
-                })
-                var dest = path.join(spriteDestDIR, itemDIR)
-                var sprite = setUpSpriteConfig(options, src, dest)
-                return sprite;
-            })
-
-            retinaSpritelist = listDIR(spriteSrcDIR).filter(function(itemDIR){
-                return !!listFileInclude(path.join(spriteSrcDIR, itemDIR), include).length
-            }).map(function (itemDIR) {
-                var src = listFileInclude(path.join(spriteSrcDIR, itemDIR), include).map(function(sliceFile){
-                    return path.join(spriteSrcDIR, itemDIR, sliceFile);
-                })
-                var dest = path.join(spriteDestDIR, itemDIR+RETINA_SUFFIX)
-                var sprite = setUpSpriteConfig(options, src, dest)
-                return sprite
-            })
-
-        }
-        if(listFileExclude(spriteSrcDIR, exclude).length>0){
-            file.mkdirsSync(cssDestDIR)
-            file.mkdirsSync(spriteDestDIR)
-            var src = listFileExclude(spriteSrcDIR, exclude).map(function(sliceFile){
-                return path.join(spriteSrcDIR, sliceFile);
-            })
-            var dest = path.join(spriteDestDIR, 'sprite');
-            var sprite = setUpSpriteConfig(options, src, dest)
-            spritelist.push(sprite);
-        }
-        if(listFileInclude(spriteSrcDIR, include).length>0){
-            var src = listFileInclude(spriteSrcDIR, include).map(function(sliceFile){
-                return path.join(spriteSrcDIR, sliceFile);
-            })
-            var dest = path.join(spriteDestDIR, 'sprite'+RETINA_SUFFIX);
-            var sprite = setUpSpriteConfig(options, src, dest)
-            retinaSpritelist.push(sprite);
-        }
-
-        csslist = listFile(cssSrcDIR).map(function (itemFile) {
+        csslist = glob.sync('./*.css', {
+            cwd: path.join(process.cwd(), cssSrcDIR)
+        }).map(function (itemFile) {
             var css = {};
-            css.src = path.join(cssSrcDIR, itemFile);
-            css.dest = path.join(cssDestDIR, itemFile);
-            return css;
-        });
+            css.src = path.join(cssSrcDIR, itemFile)
+            css.dest = path.join(cssDestDIR, itemFile)
+            return css
+        })
 
         // utils
-        function setUpSpriteConfig(dataObj, srcArr, destStr){
-            var sprite = {}
-            sprite.engine = dataObj.engine || 'auto';
-            sprite.algorithm = dataObj.algorithm || 'top-down';
-            sprite.padding = dataObj.padding || 0;
-            sprite.exportOpts = dataObj.exportOpts || {};
-            sprite.exportOpts.format = sprite.exportOpts.format || 'png';
-            sprite.src = srcArr
-            sprite.dest = path.join(destStr + '.'+sprite.exportOpts.format);
-            return sprite
-        }
-        function isInclude(str, arr){
-            var isFound = false
-            if(arr){
-                arr.forEach(function(e){
-                    if(~str.indexOf(e)){isFound = true}
+        function dir2config(dir) {
+            var _configItems = []
+            var _cwdName = path.resolve(dir) === process.cwd()? 'global': path.basename(dir)
+            var _files = glob.sync(dir+'+(*.png|*.jpg|*.gif)', {
+                cwd: path.join(process.cwd(), spriteSrcDIR),
+                nodir: true
+            })
+            if(_files.length === 0){return _configItems}
+
+            var splitRst = splitByRule(_files)
+            _configItems = splitRst.map(function (e) {
+                var _sprite_suffix = (e.dest==='@1x'?'':e.dest)
+                var _sprite_dest = path.join(process.cwd(), spriteDestDIR, _cwdName + _sprite_suffix)
+                var _sprite_src = e.src.map(function (ee) {
+                    return path.join(process.cwd(), spriteSrcDIR, ee)
                 })
+                return formatSpriteSmithCfg(_sprite_dest, _sprite_src)
+            })
+            return _configItems
+        }
+
+        function splitByRule(slices) {
+            var arr = [
+                {
+                    "dest": "@1x",
+                    "src" : _.filter(slices, function (e) {
+                        return !~e.indexOf('@')
+                    })
+                },
+                {
+                    "dest": RETINA_2X_SUFFIX,
+                    "src" : _.filter(slices, function (e) {
+                        return !!~e.indexOf(RETINA_2X_SUFFIX)
+                    })
+                },
+                {
+                    "dest": RETINA_3X_SUFFIX,
+                    "src" : _.filter(slices, function (e) {
+                        return !!~e.indexOf(RETINA_3X_SUFFIX)
+                    })
+                }
+            ]
+            arr = _.filter(arr, function (e) {
+                return !_.isEmpty(e.src)
+            })
+            return arr
+        }
+
+        function formatSpriteSmithCfg(_dest,_src) {
+            var exportImageFormat = (options.exportOpts && options.exportOpts.format) || 'png';
+            return {
+                "engine": options.engine || "gm",
+                "algorithm": options.algorithm || 'top-down',
+                "padding": options.padding || 0,
+                "dest": _dest+'.'+exportImageFormat,
+                "exportOpts": {
+                    "format": exportImageFormat,
+                    "quality": (options.exportOpts && options.exportOpts.quality) || 90
+                },
+                "src": _src
             }
-            return isFound
         }
-        function listDIR (f) {
-            var dir = fs.lstatSync(f).isDirectory() ? f : path.dirname(f)
-            var list = fs.readdirSync(dir).filter(function (file) {
-                return fs.statSync(path.join(dir, file)).isDirectory()
-            })
-            return list
-        }
-        function listFile (f) {
-            var dir = fs.lstatSync(f).isDirectory() ? f : path.dirname(f)
-            var list = fs.readdirSync(dir).filter(function (file) {
-                return fs.statSync(path.join(dir, file)).isFile()
-            })
-            return list
-        }
-        function listFileExclude (f, exclude) {
-            var dir = fs.lstatSync(f).isDirectory() ? f : path.dirname(f)
-            var list = fs.readdirSync(dir).filter(function (file) {
-                return fs.statSync(path.join(dir, file)).isFile() && !isInclude(file, exclude)
-            })
-            return list
-        }
-        function listFileInclude (f, include) {
-            var dir = fs.lstatSync(f).isDirectory() ? f : path.dirname(f)
-            var list = fs.readdirSync(dir).filter(function (file) {
-                return fs.statSync(path.join(dir, file)).isFile() && isInclude(file, include)
-            })
-            return list
-        }
+
     })();
 
-    // Sprite creating function
+    // 2. Sprite creating function
     var _spriteSmithWrapper = function (config, callback) {
-        var sprite = config.dest;
-        delete config.sprite;
-        spritesmith(config, function (err, result) {
-            if (err) {
-                console.error(err); process.abort();
-                return callback(err);
-
-            }else {
-                fs.writeFile(sprite, result.image, { encoding: 'binary' })
-                var tmpResult = result.coordinates;
-                for (var key in result.coordinates) {
-                    var newKey = path.join(process.cwd(), key).toLowerCase();
-                    imageReplaces[ newKey ] = tmpResult[ key ];
-                    imageReplaces[ newKey ].sprite = path.join(process.cwd(), sprite);
-
-                }
-                callback(false);
-            }
-        });
-    };
-
-    // SpriteRetina creating function
-    var _retinaSpriteSmithWrapper = function (config, callback) {
         var sprite = config.dest;
         delete config.sprite;
         spritesmith(config, function (err, result) {
@@ -161,15 +120,15 @@ module.exports = function (options) {
             gm(sprite).size(function(err, size){
                 if(err){ console.error(err); process.abort(); return;}
                 if(size.width%2 ==1 || size.height%2 ==1){
-//                        console.error('warning: all slices are not allow odd pix')
+//                        console.error('warning: sprite's width is odd pix')
                 }
-                console.log('Done! [Created] -->'+sprite)
+                console.log('[Created] -->'+sprite)
                 var tmpResult = result.coordinates
                 for (var key in result.coordinates) {
-                    var newKey = path.join(process.cwd(), key).toLowerCase();
-                    retinaImageReplaces[ newKey ] = tmpResult[ key ];
-                    retinaImageReplaces[ newKey ].sprite = path.join(process.cwd(), sprite);
-                    retinaImageReplaces[ newKey ].spriteWidth = size.width;
+                    var newKey = path.relative(cssSrcDIR, key).toLowerCase();
+                    imageReplaces[ newKey ] = tmpResult[ key ];
+                    imageReplaces[ newKey ].sprite = path.relative(path.join(process.cwd(), cssDestDIR), sprite).replace(/\\/ig, '/');
+                    imageReplaces[ newKey ].spriteWidth = size.width;
                 }
                 callback(false);
             })
@@ -178,50 +137,59 @@ module.exports = function (options) {
 
     };
 
-    // Replace stylesheet
+    // 3. Replace stylesheet
     var _insertSprites = function (css) {
 
-        var retinaCssSnippetBanner = (function(){/*
+        var cssSnippetBanner2x = (function(){/*
+
          @media only screen and (-webkit-min-device-pixel-ratio: 1.5),only screen and (min--moz-device-pixel-ratio: 1.5),(min-resolution: 1.5dppx),only screen and (min-resolution: 240dpi) {
          */}).toString().split('\n').slice(1, -1).join('\n');
-        var retinaCssSnippetFooter = (function(){/*
+        var cssSnippetFooter2x = (function(){/*
          }
          */}).toString().split('\n').slice(1, -1).join('\n');
-        var retinaCssSnippet = '';
+        var cssSnippet2x = '';
+
+
+        var cssSnippetBanner3x = (function(){/*
+
+         @media only screen and (min-device-width : 414px) and (max-device-width : 736px) and (-webkit-min-device-pixel-ratio : 3) {
+         */}).toString().split('\n').slice(1, -1).join('\n');
+        var cssSnippetFooter3x = (function(){/*
+         }
+         */}).toString().split('\n').slice(1, -1).join('\n');
+        var cssSnippet3x = '';
 
         var regex = new RegExp('background-image:[\\s]?url\\(["\']?(?!http[s]?|/)([\\w\\d\\s!./\\-\\_@]*\\.[\\w?#]+)["\']?\\)', 'ig'),
-            regex2 = new RegExp('(.*:hover.*)background-image:[\\s]?url\\(["\']?(?!http[s]?|/)([\\w\\d\\s!./\\-\\_]*\\.[\\w?#]+)["\']?\\)[^;]*;', 'ig'),
-            dir = path.join(process.cwd(), path.dirname(css.src)),
-            destDIR = path.join(process.cwd(), path.dirname(css.dest)),
             data = fs.readFileSync(css.src, 'utf8'),
             resources = _.uniq(data.match(regex)),
-            pathToResource,
-            absolutePath,
-            newPath,
-            img;
+            pathToResource, img, selectors;
 
         if (resources !== null) {
             for (var i = 0; i < resources.length; i++) {
-                pathToResource = resources[i].replace(regex, '$1');
-                absolutePath = path.join(dir, pathToResource).toLowerCase();
-                img = retinaImageReplaces[addSuffix(absolutePath, RETINA_SUFFIX)]
+                pathToResource = resources[i].replace(regex, '$1')
+                selectors = ''
+
+                img = imageReplaces[addSuffix(pathToResource, RETINA_3X_SUFFIX)]
                 if (img) {
-                    newPath = path.relative(destDIR, img.sprite).replace(/\\/ig, '/')
-                    var selectors = getSelectorsBySlice(data, pathToResource)
-                    retinaCssSnippet += calcRetinaCssSnippet(selectors, img, newPath)
-                    retinaImageReplaces[addSuffix(absolutePath, RETINA_SUFFIX)] = null
+                    selectors = selectors || getSelectorsBySlice(data, pathToResource)
+                    cssSnippet3x += calcRetinaCssSnippet(selectors, img)
                 }
-                img = imageReplaces[ absolutePath ]
+                img = imageReplaces[addSuffix(pathToResource, RETINA_2X_SUFFIX)]
                 if (img) {
-                    newPath = path.relative(destDIR, img.sprite).replace(/\\/ig, '/')
-                    data = calcCssSnippet(data, pathToResource, img, newPath)
+                    selectors = selectors || getSelectorsBySlice(data, pathToResource)
+                    cssSnippet2x += calcRetinaCssSnippet(selectors, img)
+                }
+                img = imageReplaces[ pathToResource ]
+                if (img) {
+                    data = calcCssSnippet(data, pathToResource, img)
                 }
             }
         }
-        // remove :hover class's background-image
-        // data = data.replace(regex2, '$1 ');
-        if(retinaCssSnippet.length>0){
-            data += retinaCssSnippetBanner + retinaCssSnippet + retinaCssSnippetFooter
+        if(cssSnippet2x.length>0){
+            data += cssSnippetBanner2x + cssSnippet2x + cssSnippetFooter2x
+        }
+        if(cssSnippet3x.length>0){
+            data += cssSnippetBanner3x + cssSnippet3x + cssSnippetFooter3x
         }
         fs.writeFile(css.dest, data)
 
@@ -232,14 +200,14 @@ module.exports = function (options) {
             var newname = path.basename(url, path.extname(url))+suffix+path.extname(url)
             return path.join(dir, newname)
         }
-        function calcCssSnippet(css, url, img, newPath){
+        function calcCssSnippet(css, url, img){
             var regExp = new RegExp('background-image:[\\s]?url\\(["\']?'+url+'["\']?\\)', 'ig')
-            var pos = 'background-image:url("' + newPath +'"); background-position: -'+img.x+'px -'+img.y+'px'
+            var pos = 'background-image:url("' + img.sprite +'"); background-position: -'+img.x+'px -'+img.y+'px'
             return css.replace(regExp, pos)
         }
-        function calcRetinaCssSnippet(selectors, img, newPath){
+        function calcRetinaCssSnippet(selectors, img){
             return selectors.map(function(selector){
-                var snippet = selector+'{background-image:url("' + newPath +'");' +
+                var snippet = selector+'{background-image:url("' + img.sprite +'");' +
                     'background-position: -'+img.x/2+'px -'+img.y/2+'px; background-size: '+ img.spriteWidth/2 +'px; }'
                 return snippet
             }).join('')
@@ -255,14 +223,11 @@ module.exports = function (options) {
         return;
     };
 
-    // Process starter
-    async.eachSeries(retinaSpritelist, _retinaSpriteSmithWrapper, function(err){
-        async.eachSeries(spritelist, _spriteSmithWrapper, function(err){
-            for (var j = 0; j < csslist.length; j++) {
-                _insertSprites(csslist[ j ]);
-            }
-            successCB();
-        })
+    // 0. main
+    async.eachSeries(configItems, _spriteSmithWrapper, function(err){
+        for (var j = 0; j < csslist.length; j++) {
+            _insertSprites(csslist[ j ]);
+        }
+        successCB();
     })
-
 };
--- test/testMultiSprite.js
+++ test/testMultiSprite.js
@@ -9,13 +9,13 @@ var multiSprite = require('./../index')
  * cd multi-sprite/test/ ,then exec 'node testMultiSprite'
  */
 var ops = {
-    srcCss: 'web/src/css',
+    srcCss: 'test/web/src/css',
     // List of images to add to sprite (each sub-folder generate 1 image)
-    srcImg: 'web/src/slice',
+    srcImg: 'test/web/src/slice',
     // Target css file folder, can be the same as source
-    destCss: 'web/dest/css',
+    destCss: 'test/web/dest/css',
     // Address of target image's folder, target image's name = source folder's name
-    destImg: 'web/dest/img/sprite',
+    destImg: 'test/web/dest/img/sprite',
     // OPTIONAL: Image placing algorithm: top-down, left-right, diagonal, alt-diagonal, binary-tree
     'algorithm': 'binary-tree',
     // OPTIONAL: Padding between imagesm
@@ -34,3 +34,45 @@ var ops = {
     }
 }
 multiSprite(ops)
+
+
+
+//var spritesmith = require('spritesmith');
+//config = {
+//    "engine": "gm",
+//    "algorithm": "binary-tree",
+//    "padding": 4,
+//    "dest": "test/web/dest/img/sprite/s4game.png",
+//    "exportOpts": {
+//        "format": "png",
+//        "quality": 90
+//    },
+//    padding: 4,
+//    src:[
+//        "test/web/src/slice/s4game/icon-play-disable.png",
+//        "test/web/src/slice/s4game/icon-play-live.png"
+//    ]
+//}
+//spritesmith(config,function(err, result){
+//    if (err) { console.error(err); process.abort(); return callback(err); }
+//    fs.writeFile(config.dest, result.image, { encoding: 'binary' })
+//})
+
+
+var imageReplaces = {
+    "/users/adi/duowan/github/multi-sprite/test/web/src/slice/s4game/icon-play-disable@2x.png":{
+        height: 32,
+        width: 44,
+        sprite: "/Users/adi/duowan/github/multi-sprite/test/web/dest/img/sprite/s4game@2x.png",
+        spriteWidth: 128,
+        x: 0,
+        y: 0
+    }
+}
+
+
+
+
+
+
+
