75495627f147ad148427fc868ec94b888578e1d7
twlk28 <twlk28@gmail.com>
2015-01-05 19:04:05 +0800

    add test file

--- index.js
+++ index.js
@@ -1,121 +1,22 @@
 var spritesmith = require('spritesmith'),
     fs = require('fs'),
     path = require('path'),
-    file = require('file'),
     _ = require('lodash'),
-    glob = require('glob'),
     async = require('async'),
     gm = require('spritesmith/node_modules/gmsmith/node_modules/gm');
 
-// Export the SpriteMaker function
+var getConfig = require('./lib/getConfig'),
+    updateCss = require('./lib/updateCss');
+
 module.exports = function (options) {
     "use strict";
 
-    var configItems = [],
-        csslist = [],
-        imageReplaces = {},
-        RETINA_2X_SUFFIX = '@2x',
-        RETINA_3X_SUFFIX = '@3x';
+    var successCB = options.successCB || function(){}
+    var configs = getConfig(options)
+    var imageReplaces = {}
 
-    // Normalize data
-    var successCB = options.successCB || function(){},
-        spriteSrcDIR = options.srcImg,
-        spriteDestDIR = options.destImg,
-        cssSrcDIR = options.srcCss,
-        cssDestDIR = options.destCss;
-
-    // 1. Prepare spirteConfigData
-    (function () {
-        configItems = configItems.concat(dir2config('./'))
-        glob.sync('./*/', {
-            cwd: path.join(process.cwd(), spriteSrcDIR)
-        }).forEach(function (subdir) {
-            configItems = configItems.concat(dir2config(subdir))
-        })
-        configItems = _.filter(configItems, function(e){
-            return !_.isEmpty(e)
-        })
-
-        csslist = glob.sync('./*.css', {
-            cwd: path.join(process.cwd(), cssSrcDIR)
-        }).map(function (itemFile) {
-            var css = {};
-            css.src = path.join(cssSrcDIR, itemFile)
-            css.dest = path.join(cssDestDIR, itemFile)
-            return css
-        })
-
-        file.mkdirsSync(cssDestDIR)
-        file.mkdirsSync(spriteDestDIR)
-
-        // utils
-        function dir2config(dir) {
-            var _configItems = []
-            var _cwdName = path.resolve(dir) === process.cwd()? 'global': path.basename(dir)
-            var _files = glob.sync(dir+'+(*.png|*.jpg|*.gif)', {
-                cwd: path.join(process.cwd(), spriteSrcDIR),
-                nodir: true
-            })
-            if(_files.length === 0){return _configItems}
-
-            var splitRst = splitByRule(_files)
-            _configItems = splitRst.map(function (e) {
-                var _sprite_suffix = (e.dest==='@1x'?'':e.dest)
-                var _sprite_dest = path.join(process.cwd(), spriteDestDIR, _cwdName + _sprite_suffix)
-                var _sprite_src = e.src.map(function (ee) {
-                    return path.join(process.cwd(), spriteSrcDIR, ee)
-                })
-                return formatSpriteSmithCfg(_sprite_dest, _sprite_src)
-            })
-            return _configItems
-        }
-
-        function splitByRule(slices) {
-            var arr = [
-                {
-                    "dest": "@1x",
-                    "src" : _.filter(slices, function (e) {
-                        return !~e.indexOf('@')
-                    })
-                },
-                {
-                    "dest": RETINA_2X_SUFFIX,
-                    "src" : _.filter(slices, function (e) {
-                        return !!~e.indexOf(RETINA_2X_SUFFIX)
-                    })
-                },
-                {
-                    "dest": RETINA_3X_SUFFIX,
-                    "src" : _.filter(slices, function (e) {
-                        return !!~e.indexOf(RETINA_3X_SUFFIX)
-                    })
-                }
-            ]
-            arr = _.filter(arr, function (e) {
-                return !_.isEmpty(e.src)
-            })
-            return arr
-        }
-
-        function formatSpriteSmithCfg(_dest,_src) {
-            var exportImageFormat = (options.exportOpts && options.exportOpts.format) || 'png';
-            return {
-                "engine": options.engine || "gm",
-                "algorithm": options.algorithm || 'top-down',
-                "padding": options.padding || 0,
-                "dest": _dest+'.'+exportImageFormat,
-                "exportOpts": {
-                    "format": exportImageFormat,
-                    "quality": (options.exportOpts && options.exportOpts.quality) || 90
-                },
-                "src": _src
-            }
-        }
-
-    })();
-
-    // 2. Sprite creating function
-    var _spriteSmithWrapper = function (config, callback) {
+    // Sprite creating function
+    function _spriteSmithWrapper(config, callback) {
         var sprite = config.dest;
         delete config.sprite;
         spritesmith(config, function (err, result) {
@@ -123,15 +24,12 @@ module.exports = function (options) {
             fs.writeFile(sprite, result.image, { encoding: 'binary' })
             gm(sprite).size(function(err, size){
                 if(err){ console.error(err); process.abort(); return;}
-                if(size.width%2 ==1 || size.height%2 ==1){
-//                        console.error('warning: sprite's width is odd pix')
-                }
                 console.log('[Created] -->'+sprite)
                 var tmpResult = result.coordinates
                 for (var key in result.coordinates) {
-                    var newKey = path.relative(cssSrcDIR, key).toLowerCase();
+                    var newKey = path.relative(options.srcCss, key).toLowerCase();
                     imageReplaces[ newKey ] = tmpResult[ key ];
-                    imageReplaces[ newKey ].sprite = path.relative(path.join(process.cwd(), cssDestDIR), sprite).replace(/\\/ig, '/');
+                    imageReplaces[ newKey ].sprite = path.relative(path.join(process.cwd(), options.destCss), sprite).replace(/\\/ig, '/');
                     imageReplaces[ newKey ].spriteWidth = size.width;
                 }
                 callback(false);
@@ -139,99 +37,14 @@ module.exports = function (options) {
 
         });
 
-    };
+    }
 
-    // 3. Replace stylesheet
-    var _insertSprites = function (css) {
-
-        var cssSnippetBanner2x = (function(){/*
-
-@media only screen and (-webkit-min-device-pixel-ratio: 1.5),only screen and (min--moz-device-pixel-ratio: 1.5),(min-resolution: 1.5dppx),only screen and (min-resolution: 240dpi) {
-         */}).toString().split('\n').slice(1, -1).join('\n');
-        var cssSnippetFooter2x = (function(){/*
-         }
-         */}).toString().split('\n').slice(1, -1).join('\n');
-        var cssSnippet2x = '';
-
-
-        var cssSnippetBanner3x = (function(){/*
-
-@media only screen and (min-device-width : 414px) and (max-device-width : 736px) and (-webkit-min-device-pixel-ratio : 3) {
-         */}).toString().split('\n').slice(1, -1).join('\n');
-        var cssSnippetFooter3x = (function(){/*
-         }
-         */}).toString().split('\n').slice(1, -1).join('\n');
-        var cssSnippet3x = '';
-
-        var regex = new RegExp('background-image:[\\s]?url\\(["\']?(?!http[s]?|/)([\\w\\d\\s!./\\-\\_@]*\\.[\\w?#]+)["\']?\\)', 'ig'),
-            data = fs.readFileSync(css.src, 'utf8'),
-            resources = _.uniq(data.match(regex)),
-            pathToResource, img, selectors;
-
-        if (resources !== null) {
-            for (var i = 0; i < resources.length; i++) {
-                pathToResource = resources[i].replace(regex, '$1')
-                selectors = ''
-
-                img = imageReplaces[addSuffix(pathToResource, RETINA_3X_SUFFIX)]
-                if (img) {
-                    selectors = selectors || getSelectorsBySlice(data, pathToResource)
-                    cssSnippet3x += calcRetinaCssSnippet(selectors, img)
-                }
-                img = imageReplaces[addSuffix(pathToResource, RETINA_2X_SUFFIX)]
-                if (img) {
-                    selectors = selectors || getSelectorsBySlice(data, pathToResource)
-                    cssSnippet2x += calcRetinaCssSnippet(selectors, img)
-                }
-                img = imageReplaces[ pathToResource ]
-                if (img) {
-                    data = calcCssSnippet(data, pathToResource, img)
-                }
-            }
-        }
-        if(cssSnippet2x.length>0){
-            data += cssSnippetBanner2x + cssSnippet2x + cssSnippetFooter2x
-        }
-        if(cssSnippet3x.length>0){
-            data += cssSnippetBanner3x + cssSnippet3x + cssSnippetFooter3x
-        }
-        fs.writeFile(css.dest, data)
-
-        // utils
-        function addSuffix(url, suffix){
-            if(!!~(url.indexOf(suffix))){return url}
-            var dir = path.dirname(url)
-            var newname = path.basename(url, path.extname(url))+suffix+path.extname(url)
-            return path.join(dir, newname)
-        }
-        function calcCssSnippet(css, url, img){
-            var regExp = new RegExp('background-image:[\\s]?url\\(["\']?'+url+'["\']?\\)', 'ig')
-            var pos = 'background-image:url("' + img.sprite +'"); background-position: -'+img.x+'px -'+img.y+'px'
-            return css.replace(regExp, pos)
-        }
-        function calcRetinaCssSnippet(selectors, img){
-            return selectors.map(function(selector){
-                var snippet = selector+'{background-image:url("' + img.sprite +'");' +
-                    'background-position: -'+img.x/2+'px -'+img.y/2+'px; background-size: '+ img.spriteWidth/2 +'px; }'
-                return snippet
-            }).join('')
-        }
-        function getSelectorsBySlice(css, url){
-            var regStr = '([\\#\\.\\w\\d\\s-_\\:\\,]*)\\{[^}]+'+url
-            var regExp = new RegExp(regStr, 'ig')
-            var matches = css.match(regExp).map(function(e){
-                return e.replace(regExp, '$1')
-            })
-            return matches
-        }
-        return;
-    };
-
-    // 0. main
-    async.eachSeries(configItems, _spriteSmithWrapper, function(err){
+    // main
+    async.eachSeries(configs, _spriteSmithWrapper, function(err){
         for (var j = 0; j < csslist.length; j++) {
-            _insertSprites(csslist[ j ]);
+            updateCss(csslist[j], imageReplaces);
         }
         successCB();
     })
-};
+
+}
---
+++ test/test.js
@@ -0,0 +1,118 @@
+var _ = require('lodash'),
+    log = console.log;
+
+module.exports = {
+    setUp: function (callback) {
+        this.cwd = process.cwd()
+        this.CONST = {}
+        this.CONST.SUFFIX_2X = '@2x'
+        this.CONST.SUFFIX_3X = '@3x'
+        this.options = {
+            srcCss: 'test/web/src/css',
+            srcImg: 'test/web/src/slice',
+            destCss: 'test/web/dest/css',
+            destImg: 'test/web/dest/img/sprite',
+            'algorithm': 'binary-tree',
+            'padding': 4,
+            'engine': 'gm',
+            'exportOpts': {
+                'format': 'png',
+                'quality': 90
+            },
+            successCB: function () {
+                log('all done!')
+            }
+        }
+        callback();
+    },
+    tearDown: function (callback) {
+        // clean up
+        callback();
+    },
+    //testSprite: function (test) {
+    //    var mod, input, output;
+    //    mod = require('../lib/getConfig')
+    //    input = {}
+    //    output = mod(input).a
+    //    test.equals(output, 1);
+    //    test.done();
+    //},
+    //testCss: function (test) {
+    //    var mod, input, output;
+    //    mod = require('../lib/getConfig')
+    //    input = {}
+    //    output = mod(input).a
+    //    test.equals(output, 1);
+    //    test.done();
+    //},
+    //testMain: function (test) {
+    //    var mod, input, output;
+    //    mod = require('../lib/getConfig')
+    //    input = {}
+    //    output = mod(input).a
+    //    test.equals(output, 1);
+    //    test.done();
+    //},
+    testGetConfig: function (test) {
+        var mod, input, output, output_expect;
+        mod = require('../lib/getConfig')
+        output = mod(this.options, this.CONST)
+        output_expect = [
+            {
+                "engine": "gm",
+                "algorithm": "binary-tree",
+                "padding": 4,
+                "dest": this.cwd+"/test/web/dest/img/sprite/s4game.png",
+                "exportOpts": {
+                    "format": "png",
+                    "quality": 90
+                },
+                "src": [
+                    this.cwd+"/test/web/src/slice/s4game/icon-play-disable.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-play-live.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-play.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-play2.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-vs.png"
+                ]
+            },
+            {
+                "engine": "gm",
+                "algorithm": "binary-tree",
+                "padding": 4,
+                "dest": this.cwd+"/test/web/dest/img/sprite/s4game@2x.png",
+                "exportOpts": {
+                    "format": "png",
+                    "quality": 90
+                },
+                "src": [
+                    this.cwd+"/test/web/src/slice/s4game/icon-play-disable@2x.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-play-live@2x.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-play2@2x.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-play@2x.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-vs@2x.png"
+                ]
+            },
+            {
+                "engine": "gm",
+                "algorithm": "binary-tree",
+                "padding": 4,
+                "dest": this.cwd+"/test/web/dest/img/sprite/s4game@3x.png",
+                "exportOpts": {
+                    "format": "png",
+                    "quality": 90
+                },
+                "src": [
+                    this.cwd+"/test/web/src/slice/s4game/icon-play-disable@3x.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-play-live@3x.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-play2@3x.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-play@3x.png",
+                    this.cwd+"/test/web/src/slice/s4game/icon-vs@3x.png"
+                ]
+            }
+        ]
+        test.equals(JSON.stringify(output), JSON.stringify(output_expect));
+        test.done();
+    }
+};
+
+
